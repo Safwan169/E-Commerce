a58642cb01c1e2bef5b00386a2c888af8d9b3581
safwan 169 <safwanhossainmridul@gmail.com>
2024-08-18 18:45:59 +0600

    footer done

--- src/Main-Layout/Main_Layout.jsx
+++ src/Main-Layout/Main_Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Componants/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Componants/Footer';
 
 const Main_Layout = () => {
     return (
         <>
         <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
             
         </>
     );
