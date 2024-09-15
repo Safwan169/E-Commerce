a6ef2d6272c5d02bcdb38154f3aa1622b9e8fe4f
safwan 169 <safwanhossainmridul@gmail.com>
2024-09-16 00:14:36 +0600

    contract clear

--- src/Componants/Contract.jsx
+++ src/Componants/Contract.jsx
@@ -3,12 +3,12 @@ import Test from './LOGIN_&_SignUp/test';
 
 const Contract = () => {
 
-    const [data] = Test()
+    // const [data] = Test()
     console.log(data)
     return (
 
         <div className='text-4xl mt-1/2 h-svh flex justify-center items-center text-gray-400 font-semibold'>
-            ON WORKING...{data?.length}
+            {/* ON WORKING...{data?.length} */}
         </div>
 
     );
