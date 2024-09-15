be397db1593e935dd332277d4133f24fbead7a89
safwan 169 <safwanhossainmridul@gmail.com>
2024-09-16 00:17:51 +0600

    check

--- src/Componants/LOGIN_&_SignUp/Test.jsx
+++ src/Componants/LOGIN_&_SignUp/Test.jsx
@@ -6,7 +6,7 @@ import {useQuery} from 'react-query'
 
 const Test = () => {
     const {user}=useContext(context)
-    // console.log(user?.email)
+    console.log(user?.email)
 
     const { refetch,isLoading,isFetching, data } = useQuery({
         queryKey: ['repoData'],
